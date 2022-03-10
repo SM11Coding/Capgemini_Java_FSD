3d6e91b2ad7d74b8376f0f44ae2ac63dba9bd3fc
Soumyadeep Maji <101235503+SoumyadeepMaji11@users.noreply.github.com>
2022-03-10 12:32:14 +0530

    Add files via upload

---
+++ Java_Arrays/Array2d.java
@@ -0,0 +1,13 @@
+package com.cg;
+
+public class Array2d {
+	public static void main(String[] args)
+	{
+
+		int[][] arr = { { 1, 2 }, { 3, 4 } };
+
+		System.out.println("arr[0][0] = " + arr[0][0]);
+	}
+
+
+}
