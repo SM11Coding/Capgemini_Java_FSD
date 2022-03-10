9c82de70d62725cea5654ae6199ef51feba48ecd
Soumyadeep Maji <101235503+SoumyadeepMaji11@users.noreply.github.com>
2022-03-10 13:29:07 +0530

    Add files via upload

---
+++ Java_Basics/TypeCasting.java
@@ -0,0 +1,19 @@
+package com.cg;
+
+public class TypeCasting {
+	public static void main(String[] Args)
+	{
+		int i = 5;
+		float f = 3.5f;
+		double d = 5.7;
+		long l= 10;
+		
+		l = i;
+		System.out.println("Int " + l);
+		
+		i = (int)d;
+		System.out.println("Int " + i);
+		
+	}
+
+}
